b776a678d74f6a5ad69f0c2cc46d7e9cf97c8882
Rubayth Haque <rubayth19@gmail.com>
2019-03-06 22:58:08 -0500

    Updates

---
+++ precache-manifest.812040aa4c11c79675346600e99533a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "6a05eb6cb7d034d3ccb0",
+    "url": "/RoboFriends/static/js/runtime~main.6a05eb6c.js"
+  },
+  {
+    "revision": "db6cad0b18f792a1b2be",
+    "url": "/RoboFriends/static/js/main.db6cad0b.chunk.js"
+  },
+  {
+    "revision": "32dda54c8cdecc4f2b21",
+    "url": "/RoboFriends/static/js/2.32dda54c.chunk.js"
+  },
+  {
+    "revision": "db6cad0b18f792a1b2be",
+    "url": "/RoboFriends/static/css/main.0434d382.chunk.css"
+  },
+  {
+    "revision": "32dda54c8cdecc4f2b21",
+    "url": "/RoboFriends/static/css/2.2841f65e.chunk.css"
+  },
+  {
+    "revision": "c4fa9a62e928e71c26bf327e16048be2",
+    "url": "/RoboFriends/index.html"
+  }
+];
